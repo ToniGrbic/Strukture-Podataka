69708eb5d068c09523ff0a5df974fa7632b09f1b
ToniGrbic <tonigrbic.5@gmail.com>
2021-10-30 20:00:10 +0200

    ZDK_2 i ZDK_3

--- ZDK_2/zdk2.c
+++ ZDK_2/zdk2.c
@@ -2,6 +2,7 @@
 #include "stdlib.h"
 #include "string.h"
 #define MAX (40)
+#define MAX_CHAR (1024)
 
 struct _Student;
 typedef struct _Student *Pozicija;
@@ -14,28 +15,45 @@ typedef struct _Student{
 
 int UnesiNaPocetak(Pozicija P, char *ime, char *prezime, int godinaRodenja);
 int UnesiNaKraj(Pozicija P, char *ime, char *prezime, int godinaRodenja);
-int IspisiListu(Pozicija P);
+int UnesiSortirano(Pozicija P, char *ime, char *prezime, int godinaRodenja);
+int UnesiNakonStud(Pozicija P, char *ime, char *prezime, int godinaRodenja, char *prezimePrije);
+int UnesiPrijeStud(Pozicija P, char *ime, char *prezime, int godinaRodenja, char *prezimeNakon);
 int PretraziPoPrezimenu(Pozicija P, char *prezime);
 Student* UnesiPodatke(char *ime, char *prezime, int godinaRodenja);
 Student* TraziPrethodnog(Pozicija P, char *prezime);
 Student* TraziTrenutnog(Pozicija P, char *prezime);
 int InsertNakon(Pozicija P, Pozicija noviStudent);
+int IspisiListu(Pozicija P);
 int IzbrisiStudenta(Pozicija P, char* prezime);
+int IzbrisiListu(Pozicija P);
+int Izbrisi(Pozicija P);
+int UnesiListuUDatoteku(Pozicija P, char *datoteka);
+int UcitajListuIzDatoteke(Pozicija P, char *datoteka);
 
 int main()
 {
     Student Head = {.ime = " ", .prezime = " ", .godinaRodenja= 0, .next = NULL};
     Pozicija P = &Head;
-
-    UnesiNaPocetak(P,"toni", "grbic", 2001);
+    Student Head2 = {.ime = " ", .prezime = " ", .godinaRodenja= 0, .next = NULL};
+    Pozicija Q = &Head2;
+    /*UnesiNaPocetak(P,"toni", "grbic", 2001);
     UnesiNaPocetak(P,"mate", "matic", 2000);
     UnesiNaKraj(P,"ivo", "ivic", 2002);
+    UnesiNakonStud(P, "ante", "baric", 1999, "matic");
+    UnesiPrijeStud(P, "mijo", "mijic", 1999, "matic");
+    IspisiListu(P->next);*/
 
+    UnesiSortirano(P, "ivo", "zdeslav", 2001);
+    UnesiSortirano(P, "toni", "grbic", 2001);
+    UnesiSortirano(P, "ana", "anjic", 1999);
+    UnesiSortirano(P, "ante", "baric", 2002);
+    UnesiSortirano(P, "mate", "matic", 2000);
     IspisiListu(P->next);
-    PretraziPoPrezimenu(P, "matic");
-    PretraziPoPrezimenu(P, "senta");
+    UnesiListuUDatoteku(P->next, "osobeUnos.txt");
     
-    IzbrisiStudenta(P, "grbic");
+    //PretraziPoPrezimenu(P, "matic");
+    //PretraziPoPrezimenu(P, "senta");
+    /*IzbrisiStudenta(P, "grbic");
     IspisiListu(P->next);
     IzbrisiStudenta(P, "matic");
     IspisiListu(P->next);
@@ -44,7 +62,12 @@ int main()
     IspisiListu(P->next);
     //brisanje zadnjeg elementa
     IzbrisiStudenta(P, "ivic");
-    IspisiListu(P->next);
+    IspisiListu(P->next);*/
+    UcitajListuIzDatoteke(Q, "osobe.txt");
+    IspisiListu(Q->next);
+    IzbrisiListu(Q);
+    IspisiListu(Q->next);
+    
     return 0;
 }
 
@@ -66,6 +89,36 @@ int UnesiNaKraj(Pozicija P, char *ime, char *prezime, int godinaRodenja)
     }
     UnesiNaPocetak(P, ime, prezime, godinaRodenja);
     return 0;
+}
+
+int UnesiNakonStud(Pozicija P, char *ime, char *prezime, int godinaRodenja, char *prezimePrije)
+{
+    P = TraziTrenutnog(P, prezimePrije);
+    if(!P){
+        printf("Student %s nepostoji\n", prezime);
+        return 1;
+    }
+    UnesiNaPocetak(P, ime, prezime, godinaRodenja);
+    return 0;
+}
+
+int UnesiPrijeStud(Pozicija P, char *ime, char *prezime, int godinaRodenja, char *prezimeNakon)
+{
+    P = TraziPrethodnog(P, prezimeNakon);
+    if(!P){
+        printf("Student %s nepostoji\n", prezime);
+        return 1;
+    }
+    UnesiNaPocetak(P, ime, prezime, godinaRodenja);
+    return 0;
+}
+
+int UnesiSortirano(Pozicija P, char *ime, char *prezime, int godinaRodenja)
+{
+    while(P->next != NULL && strcmp(P->next->prezime, prezime)<0){
+        P=P->next;
+    }
+    UnesiNaPocetak(P, ime, prezime, godinaRodenja);
 }
 
 int IspisiListu(Pozicija P)
@@ -93,17 +146,45 @@ int InsertNakon(Pozicija P, Pozicija noviStudent)
 
 int IzbrisiStudenta(Pozicija P, char* prezime)
 {
-    Pozicija temp;
     P = TraziPrethodnog(P, prezime);
     if(!P){
         printf("u listi nepostoji student %s, brisanje neuspjesno\n", prezime);
         return 1;
     }
+    Izbrisi(P);
+    return 0;
+}
+
+int IzbrisiListu(Pozicija P)
+{
+    if(P->next = NULL){
+        printf("lista je prazna\n");
+    }
+    while(P->next != NULL)
+    {
+        Izbrisi(P);
+    }
+    return 0;
+}
+
+int Izbrisi(Pozicija P){
+    Pozicija temp = NULL;
     temp = P->next;
     P->next = P->next->next;
     free(temp);
-    return 0;
 }
+
+int PretraziPoPrezimenu(Pozicija P, char *prezime)
+{
+    Pozicija Stud = NULL;
+    Stud = TraziTrenutnog(P, prezime);
+    if(!Stud){
+        printf("student %s nepostoji\n", prezime);
+    }else{
+        printf("student %s postoji\n", Stud->prezime);
+    }
+    return 0;
+} 
 
 Student* UnesiPodatke(char *ime, char *prezime, int godinaRodenja)
 {
@@ -144,15 +225,53 @@ Student* TraziTrenutnog(Pozicija P, char *prezime)
         return NULL;
 }
 
-int PretraziPoPrezimenu(Pozicija P, char *prezime)
+int UnesiListuUDatoteku(Pozicija P, char *datoteka)
 {
-    Pozicija Stud = NULL;
-    Stud = TraziTrenutnog(P, prezime);
-    if(!Stud){
-        printf("student %s nepostoji\n", prezime);
-    }else{
-        printf("student %s postoji\n", Stud->prezime);
+    FILE *fp = NULL;
+    fp = fopen(datoteka, "w");
+    if(!fp){
+        printf("pogreska u otvaranju datoteke!\n");
+        return -1;
     }
-    
+    Pozicija temp = P;
+    while(temp != NULL){
+        fprintf(fp, "%s\t", temp->ime);
+        fprintf(fp, "%s\t", temp->prezime);
+        fprintf(fp, "%d\n", temp->godinaRodenja);
+        temp = temp->next;
+    }
+    fclose(fp);
     return 0;
 }
+
+int UcitajListuIzDatoteke(Pozicija Q, char *datoteka)
+{
+    FILE *fp = NULL;
+    fp = fopen(datoteka, "r");
+   if(!fp){
+       printf("otvaranje datoteke neuspjelo\n");
+       return -1;
+    }
+   if(Q->next != NULL){
+       printf("lista nije prazna, molimo ucitajte praznu listu\n");
+       return 1;
+    }
+    while(!feof(fp)){
+        Pozicija Stud = NULL;
+        char buffer[MAX_CHAR] = { 0 };
+        Stud = (Pozicija)malloc(sizeof(Student));
+        if(!Stud){
+            printf("greska u alokaciji memorije\n");
+            return -1;
+        }
+        fgets(buffer, MAX_CHAR, fp);
+        if(sscanf(buffer, "%s %s %d", Stud->ime, Stud->prezime, &Stud->godinaRodenja) == 3)
+        {
+            InsertNakon(Q, Stud);
+            Q = Q->next;
+        }
+    }
+    fclose(fp);
+    return 0;
+}
+
